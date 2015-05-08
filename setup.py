7d9495a62cc19ca61c270b0bf5eda36d2616fc3a
Michal Nazarewicz <mina86@mina86.com>
2015-05-08 22:17:07 +0200

    Include ‘download_url’ in setup call

--- setup.py
+++ setup.py
@@ -1,36 +1,35 @@
 from distutils.core import setup
 import os
-import version
+import re
 import sys
+import version
 
+release = version.getVersion()
 
 if len(sys.argv) == 2 and sys.argv[1] == 'builddoc':
-    release = version.getVersion()
     os.execlp('sphinx-build',
               '-Drelease=' + release,
               '-Dversion=' + '.'.join(release.split('.', 2)[0:2]),
               '.', 'html')
-
 
 with open('README.rst') as f:
     readme = f.read()
 with open('version-history.rst') as f:
     readme += '\n' + f.read()
 
-
-setup(
-    name='pygtrie',
-    version=version.getVersion(),
-    description='Trie data structure implementation.',
-    long_description=readme,
-    author='Michal Nazarewicz',
-    author_email='mina86@mina86.com',
-    url='https://github.com/google/pytrie',
-    py_modules=['trie'],
-    license='Apache-2.0',
-    platforms='Platform Independent',
-    keywords=['trie', 'prefix tree', 'data structure'],
-    classifiers=[
+kwargs = {
+    'name': 'pygtrie',
+    'version': release,
+    'description': 'Trie data structure implementation.',
+    'long_description': readme,
+    'author': 'Michal Nazarewicz',
+    'author_email': 'mina86@mina86.com',
+    'url': 'https://github.com/google/pytrie',
+    'py_modules': ['trie'],
+    'license': 'Apache-2.0',
+    'platforms': 'Platform Independent',
+    'keywords': ['trie', 'prefix tree', 'data structure'],
+    'classifiers': [
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
@@ -41,4 +40,9 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-)
+}
+
+if re.search(r'(?:\d+\.)*\d+', release):
+    kwargs['download_url'] = kwargs['url'] + '/tarball/v' + release
+
+setup(**kwargs)
