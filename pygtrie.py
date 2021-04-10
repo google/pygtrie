827173c527c2c0e92f46e8ea5996ea8b6fff1e48
Runming Lu <runming@fb.com>
2021-04-09 17:52:47 -0700

    Import collections.abc instead of collections
    Importing ABCs from collectiions is deprecated since Python 3.3 and it
    will stop working in 3.9. Use import collections.abc instead

--- pygtrie.py
+++ pygtrie.py
@@ -39,7 +39,10 @@ __author__ = 'Michal Nazarewicz <mina86@mina86.com>'
 __copyright__ = 'Copyright 2014 Google Inc.'
 
 
-import collections as _collections
+try:
+    import collections.abc as _collections
+except ImportError:
+    import collections as _collections
 
 # Python 2.x and 3.x compatibility stuff
 if hasattr(dict, 'iteritems'):
